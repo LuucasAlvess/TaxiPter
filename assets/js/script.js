19539503ed972bfb325d4066d7b8bc8901a0475c
LuucasAlvess <lucaslima2011h@gmail.com>
2023-11-17 18:16:58 -0300

    developing home

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,40 @@
-const faqItems = document.querySelectorAll('.faq-item');
+const buttons = document.querySelectorAll("[data-carousel-button]");
+const carousel = document.querySelector("[data-carousel]");
+const slides = carousel.querySelector("[data-slides]");
 
-faqItems.forEach(item => {
-  const question = item.querySelector('.faq-question');
-  const answer = item.nextElementSibling;
-  const icon = item.querySelector('i');
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    clearInterval(interval);
 
-  item.addEventListener('click', () => {
-    faqItems.forEach(otherItem => {
-      if (otherItem !== item) {
-        const otherAnswer = otherItem.nextElementSibling;
-        const otherIcon = otherItem.querySelector('i');
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
 
-        otherAnswer.classList.remove('active');
-        otherIcon.classList.remove('active');
-        otherAnswer.style.maxHeight = "0";
-      }
-    });
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
 
-    answer.classList.toggle('active');
-    icon.classList.toggle('active');
-    if (answer.classList.contains('active')) {
-      answer.style.maxHeight = answer.scrollHeight + "px";
-    } else {
-      answer.style.maxHeight = "0";
-    }
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+
+    // Defina a cor de fundo diretamente para preto
+    button.style.backgroundColor = "var(--text-dark)";
+
+    button.blur(); // Remove o foco do botão
+    startInterval(); // Reinicie o intervalo após cada clique
   });
 });
+
+
+function startInterval() {
+  interval = setInterval(() => {
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  }, 4000);
+}
+
+// Inicie o intervalo pela primeira vez
+startInterval();
